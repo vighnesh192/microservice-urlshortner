445bd4fcf7f15c5a6b2e21f9a31af5c11240d354
vighnesh192 <vighnesh192000@gmail.com>
2021-12-04 20:34:18 +0530

    Fix: Returning a frozen object

--- src/shorturl/shorturl.js
+++ src/shorturl/shorturl.js
@@ -3,9 +3,10 @@ export default function buildMakeShorturl({ validateUrl }) {
     return function makeShorturl({ original_url, short_url }) {
         try {
             const validatedUrl = validateUrl(original_url);
-            return { 
-                original_url: validatedUrl, short_url
-            }
+            return Object.freeze({
+                getOriginalUrl: () => validatedUrl,
+                getShortUrl: () => short_url
+            })
         } catch (error) {
             throw new Error(error);
         }
