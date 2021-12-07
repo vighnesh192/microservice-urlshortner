12ec992573927ca112373701f5a1a4ad392b9e8d
vighnesh192 <vighnesh192000@gmail.com>
2021-12-07 10:03:15 +0530

    Feat: Data-access: Established db connection and created Schema

---
+++ src/data-access/index.js
@@ -0,0 +1,14 @@
+// Import mongoose and establish a connection
+// Import URL Model and inject it to makeUrlDb
+
+import makeUrlDb from "./url-db.js";
+import Url from "./Url.js";
+
+import mongoose from 'mongoose';
+
+// DB Connection
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+const urlDbAccess = makeUrlDb({ Url })
---
+++ src/data-access/url-db.js
@@ -0,0 +1,4 @@
+// Needs URL Model so inject it from index.js
+export default function makeUrlDb({ url }) {
+    //Create insert, findbyid, etc methods and return frozen object
+}
