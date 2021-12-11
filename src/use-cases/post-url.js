2d215b4758570d5a38e326678addba3d8b7d549f
vighnesh192 <vighnesh192000@gmail.com>
2021-12-11 18:31:12 +0530

    Fix: Added try catch, fixed typo, fixed object being thrown

--- src/use-cases/post-url.js
+++ src/use-cases/post-url.js
@@ -3,16 +3,22 @@ import makeShorturl from '../shorturl/index.js';
 export default function makePostUrl({ urlDbAccess }) {
     // We'll get urlInfo from controller
     return async function postUrl(urlInfo) {
-        const validatedUrl = makeShorturl(urlInfo)
-        const checkIfPresent = await urlDbAccess.findOne({ originalUrl: validatedUrl.getOriginalUrl() })
-        if(!checkIfPresent){
-            return urlDbAccess.insert({ originalUrl: validatedUrl.getOriginalUrl() })
-        }
-        else {
-            throw {
-                shortUrl: checkIfPresent._id,
-                error: new Error('Url already exists')
+        try {
+            const validatedUrl = makeShorturl(urlInfo)
+            const checkIfPresent = await urlDbAccess.findOne({ original_url: validatedUrl.getOriginalUrl() })
+            if(!checkIfPresent){
+                const result = await urlDbAccess.insert({ original_url: validatedUrl.getOriginalUrl() })
+                return result;
             }
+            else {
+                throw {
+                    shortUrl: checkIfPresent.short_url,
+                    error: 'Url already exists'
+                }
+            }
+        }
+        catch(e) {
+            throw e
         }
     }
 }   
