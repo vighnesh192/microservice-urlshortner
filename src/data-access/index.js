bb71c0bf035ba06f971ed5647c187e25abb45283
vighnesh192 <vighnesh192000@gmail.com>
2021-12-14 13:40:19 +0530

    Logging a message after DB connection

--- src/data-access/index.js
+++ src/data-access/index.js
@@ -8,7 +8,7 @@ import mongoose from 'mongoose';
 
 // DB Connection
 const url = process.env.MONGO_URI
-mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true}, () => console.log('Connected to DB!!'))
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
