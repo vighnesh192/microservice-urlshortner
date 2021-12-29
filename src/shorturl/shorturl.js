a3f8aadf499b6e1be383f8651ebce5a32de0d06d
vighnesh192 <vighnesh192000@gmail.com>
2021-12-29 10:55:27 +0530

    Fix: parsing url using urlparser from node and considering ftp as invalid

--- src/shorturl/shorturl.js
+++ src/shorturl/shorturl.js
@@ -1,6 +1,9 @@
 // Injecting validateUrl so that this entity doesn't directly upon Node's dns module
 export default function buildMakeShorturl({ validateUrl }) {
     return async function makeShorturl({ original_url, short_url }) {
+        if(!(original_url.includes(':/') && (original_url.includes('https') || original_url.includes('http')))) {
+            throw 'invalid url';
+        }
         original_url = original_url.replace('https://', '')
         original_url = original_url.replace('ftp:/', '')
         let clone = original_url
@@ -11,7 +14,8 @@ export default function buildMakeShorturl({ validateUrl }) {
         }
         console.log(clone+" && "+original_url)
         try {
-            await validateUrl(clone);
+            // await validateUrl(clone);
+            await validateUrl(original_url);
         } catch (error) {
             console.log("DNS ERROR:-", error)
             throw error;
